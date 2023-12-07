6ba5e40f415bd5d08e09165f16cb6a09a05754f2
차준화 <jhcha@gsretail.com>
2023-12-07 15:47:50 +0900

    첫 커밋

---
+++ src/main/java/com/pr/github/controller/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.pr.github.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @RequestMapping("/health-check")
+    public String test() {
+        return "이상 없음";
+    }
+
+
+}
