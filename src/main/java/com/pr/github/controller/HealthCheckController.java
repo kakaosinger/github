4da027135112427f3753f4f209dcb7238d7b0bbc
최영재 <nhd5913@gsretail.com>
2023-12-07 16:12:59 +0900

    커밋
    À¸¾ƾƤ¿
    
    ÇѱÛ ÀÎÄڵù ´ٽÃ?

--- src/main/java/com/pr/github/controller/HealthCheckController.java
+++ src/main/java/com/pr/github/controller/HealthCheckController.java
@@ -11,5 +11,9 @@ public class HealthCheckController {
         return "이상 없음";
     }
 
-
+    @RequestMapping("/health-check-two-two")
+    public String test1() {
+        return "이상 없음";
+    }
+    
 }
